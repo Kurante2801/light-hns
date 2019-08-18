53627d6b00689d401eedc22ccd2ebca55f66de4e
Fafy <fabianrojas2801@gmail.com>
2019-08-18 14:21:57 -0300

    Movement convars, seeker blind and move checks

--- gamemodes/lighthns/gamemode/sh_init.lua
+++ gamemodes/lighthns/gamemode/sh_init.lua
@@ -18,6 +18,11 @@ AddCSLuaFile("sh_roundmanager.lua")
 --[[ Dynamic Tagging ]] CreateConVar("has_dyntagging", 1, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "Enable dynamic tag ranges?")
 --[[ Hiding Reward ]] CreateConVar("has_hidereward", 3, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "How many points to award hiders per round won")
 --[[ Seeker Tag Reward ]] CreateConVar("has_seekreward", 1, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "How many points to award seekers per hider tag")
+--[[ Hider Run Speed ]] CreateConVar("has_hiderrunspeed", 320, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "Speed at which hiders run at")
+--[[ Seeker Run Speed ]] CreateConVar("has_seekerrunspeed", 360, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "Speed at which seekers run at")
+--[[ Hider Walk Speed ]] CreateConVar("has_hiderwalkspeed", 190, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "Speed at which hiders walk at")
+--[[ Seeker Walk Speed ]] CreateConVar("has_seekerwalkspeed", 200, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "Speed at which seekers walk at")
+--[[ Jump Power ]] CreateConVar("has_jumppower", 210, { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED, FCVAR_SERVER_CAN_EXECUTE }, "Force everyone jumps with")
 
 function GM:CreateTeams()
 	TEAM_HIDE = 1
@@ -28,4 +33,18 @@ function GM:CreateTeams()
 
 	-- Just changing spectators colors
 	team.SetUp(TEAM_SPECTATOR, "Spectating", Color(0, 175, 100))
+end
+
+hook.Add("Tick", "HNS.SeekerBlinded", function()
+	-- See if seeker is blinded
+	if GAMEMODE.RoundState == ROUND_ACTIVE && GetConVar("has_timelimit"):GetInt() < (timer.TimeLeft("HNS.RoundTimer")) then
+		GAMEMODE.SeekerBlinded = true
+	else
+		GAMEMODE.SeekerBlinded = false
+	end
+end)
+
+function GM:Move(ply)
+	-- Prevent seekers from moving on blind time
+	return self.SeekerBlinded && ply:Team() == TEAM_SEEK
 end
--- gamemodes/lighthns/gamemode/sv_player.lua
+++ gamemodes/lighthns/gamemode/sv_player.lua
@@ -16,12 +16,96 @@ function GM:PlayerSpawn(ply)
 		ply:SetModel("models/player/group01/male_0" .. math.random(9) .. ".mdl")
 	end
 
-	-- Setting desired color shade
-	ply:SetPlayerColor(self:GetPlayerTeamColor(ply):ToVector())
+	if ply:Team() == TEAM_HIDE then
+		-- Setting desired color shade
+		ply:SetPlayerColor(self:GetTeamColor(TEAM_HIDE, ply:GetInfo("has_hidercolor", "Default")):ToVector())
+		-- Setting movement vars
+		ply:SetRunSpeed(GetConVar("has_hiderrunspeed"):GetInt())
+		ply:SetWalkSpeed(GetConVar("has_hiderwalkspeed"):GetInt())
+		-- Block flashlight
+		ply:AllowFlashlight(false)
+	else
+		-- Setting desired color shade
+		ply:SetPlayerColor(self:GetTeamColor(TEAM_SEEK, ply:GetInfo("has_seekercolor", "Default")):ToVector())
+		-- Setting movement vars
+		ply:SetRunSpeed(GetConVar("has_seekerrunspeed"):GetInt())
+		ply:SetWalkSpeed(GetConVar("has_seekerwalkspeed"):GetInt())
+		-- Allow flashlight
+		ply:AllowFlashlight(true)
+	end
+	-- Both teams get these
+	ply:SetJumpPower(GetConVar("has_jumppower"):GetInt())
+	ply:SetCrouchedWalkSpeed(0.4)
+	ply:GodEnable()
 
 	self:RoundCheck()
+end
+
+function GM:PlayerCanPickupWeapon(ply, weapon)
+	-- Allow pickup after round
+	if ply:Team() == TEAM_SPECTATOR || self.RoundState == ROUND_ACTIVE then return false end
+
+	return true
 end
 
 function GM:PlayerDisconnected(ply)
 	self:RoundCheck()
 end
+
+function GM:PlayerDeath(ply)
+	-- Award 1 frag because players lose 1 frag on death
+	ply:AddFrags(1)
+end
+
+function GM:CanPlayerSuicide(ply)
+	-- Allow seekers to suicide
+	return ply:Team() == TEAM_SEEK
+end
+
+-- Abusable doors
+local doors = {
+	["function_door_rotating"] = true,
+	["prop_door_rotating"] = true,
+}
+
+function GM:PlayerUse(ply, ent)
+	-- Stop spectators
+	if ply:Team() == TEAM_SPECTATOR then return false end
+
+	-- Anti door spam
+	if doors[ent:GetClass()] then
+		-- Stop with 1 sec delay
+		if ent.LastDoorToggle && CurTime() <= ent.LastDoorToggle + 1 then return false end
+		-- Register last time
+		ent.LastDoorToggle = CurTime()
+	end
+
+	return true
+end
+
+-- Update movement vars
+cvars.AddChangeCallback("has_hiderrunspeed", function(_, _, new)
+	for _, ply in ipairs(team.GetPlayers(TEAM_HIDE)) do
+		ply:SetRunSpeed(new)
+	end
+end)
+cvars.AddChangeCallback("has_seekerrunspeed", function(_, _, new)
+	for _, ply in ipairs(team.GetPlayers(TEAM_SEEK)) do
+		ply:SetRunSpeed(new)
+	end
+end)
+cvars.AddChangeCallback("has_hiderwalkspeed", function(_, _, new)
+	for _, ply in ipairs(team.GetPlayers(TEAM_HIDE)) do
+		ply:SetWalkSpeed(new)
+	end
+end)
+cvars.AddChangeCallback("has_seekerwalkspeed", function(_, _, new)
+	for _, ply in ipairs(team.GetPlayers(TEAM_SEEK)) do
+		ply:SetWalkSpeed(new)
+	end
+end)
+cvars.AddChangeCallback("has_jumppower", function(_, _, new)
+	for _, ply in ipairs(player.GetAll()) do
+		ply:SetJumpPower(new)
+	end
+end)
