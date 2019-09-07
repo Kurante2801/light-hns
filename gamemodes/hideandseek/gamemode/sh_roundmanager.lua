8ae46efd02fd716e7f3fa024696467f133ec73e6
Fafy <fafy@gflclan.com>
2019-09-07 17:00:20 -0300

    Refill stamina on round start

--- gamemodes/hideandseek/gamemode/sh_roundmanager.lua
+++ gamemodes/hideandseek/gamemode/sh_roundmanager.lua
@@ -110,6 +110,11 @@ if SERVER then
 			print("[LHNS] There's not enough players to begin round " .. self.RoundCount .. "!")
 		end
 
+		-- Refill stamina
+		net.Start("HNS.StaminaChange")
+			net.WriteInt(100, 8)
+		net.Broadcast()
+
 		hook.Run("HASRoundStarted")
 	end
 
