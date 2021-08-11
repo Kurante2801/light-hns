f5257a2dc6cb09d221c2b2d3987fe37cc95a9678
Fafy <fafy@gflclan.com>
2021-08-10 23:53:48 -0300

    Tweaked fonts for linux

--- gamemodes/hideandseek/gamemode/cl_fonts.lua
+++ gamemodes/hideandseek/gamemode/cl_fonts.lua
@@ -1,3 +1,5 @@
+local linux = system.IsLinux()
+
 GM.UpdateFonts = function()
     local GAMEMODE = GM or GAMEMODE
     local scale = GAMEMODE.CVars.HUDScale:GetInt()
@@ -9,13 +11,13 @@ GM.UpdateFonts = function()
     })
 
     surface.CreateFont("HNSHUD.TahomaSmall", {
-        font = "Tahoma",
+        font = linux and "Verdana Bold" or "Tahoma",
         size = 7 * scale,
         weight = 560,
     })
 
     surface.CreateFont("HNSHUD.TahomaThin", {
-        font = "Tahoma",
+        font = linux and "Verdana" or "Tahoma",
         size = 7 * scale,
         weight = 280,
     })
@@ -27,19 +29,19 @@ GM.UpdateFonts = function()
     })
 
     surface.CreateFont("HNSHUD.RobotoLarge", {
-        font = "Roboto",
+        font = linux and "Verdana" or "Roboto",
         size = 16 * scale,
         weight = 400,
     })
 
     surface.CreateFont("HNSHUD.TahomaLarge", {
-        font = "Tahoma",
+        font = linux and "Arial" or "Tahoma",
         size = 22 * scale,
         weight = 600,
     })
 
     surface.CreateFont("HNSHUD.RobotoThin", {
-        font = "Roboto",
+        font = linux and "Arial" or "Roboto",
         size = 9 * scale,
         weight = 500,
     })
@@ -49,25 +51,25 @@ GM.UpdateFonts()
 cvars.AddChangeCallback("has_hud_scale", GM.UpdateFonts, "HNS.UpdateFonts")
 
 surface.CreateFont("HNS.RobotoSmall", {
-    font = "Roboto Black",
+    font = linux and "Verdana Bold" or "Roboto Black",
     size = 18,
     weight = 550,
     antialias = true,
 })
 
 surface.CreateFont("HNS.RobotoThin", {
-    font = "Roboto",
+    font = linux and "Arial" or "Roboto",
     size = 18,
     weight = 275,
 })
 
 surface.CreateFont("HNS.RobotoLarge", {
-    font = "Roboto Black",
+    font = linux and "Verdana Bold" or "Roboto Black",
     size = 30,
 })
 
 surface.CreateFont("HNS.RobotoSpec", {
-    font = "Roboto Bold",
+    font = linux and "Verdana" or "Roboto Bold",
     size = 84,
     antialias = true,
 })
